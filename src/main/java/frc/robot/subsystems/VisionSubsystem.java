62bb719cfaea2a90d7d7d91bff28e1a76f6acba7
Bridge1234 <77758814+Bridge1234@users.noreply.github.com>
2021-03-01 21:40:52 -0500

    Added PT offset variable on Shuffleboard
    You can now set a double that controls where the green line  falls when you're running Power Tower.

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -3,6 +3,8 @@ package frc.robot.subsystems;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotContainer;
 
@@ -10,7 +12,14 @@ public class VisionSubsystem extends SubsystemBase {
     private String selectedAuto = null;
     NetworkTable visionModeTable = NetworkTableInstance.getDefault().getTable("Vision Mode");
     NetworkTableEntry selected = visionModeTable.getEntry("selected");
+
+    NetworkTableInstance ntinst = NetworkTableInstance.getDefault();
+    NetworkTable table = ntinst.getTable("PowerTower");
     
+    ShuffleboardTab tab = Shuffleboard.getTab("Power Tower");
+    NetworkTableEntry xOffset = tab.add("PT Offset", 0).getEntry();
+    
+
     public VisionSubsystem() {
         register();
         
